ba8c432ac87655d7b0aa94140a100ec5b6883c1c
Nicolas <nihco2@gmail.com>
2013-04-26 15:17:28 +0200

    add server script

---
+++ viadeo-page-crawler/index.js
@@ -0,0 +1,60 @@
+var 
+    crawlability = require('../lib/crawlability'),
+    confJSON     = require('./conf.json'),
+    conf         = require(process.cwd() + '/conf'),
+    request      = require('request'),
+    timeCounter  = require(__dirname + './../lib/timeCounter'),
+    Q            = require('q')
+;
+
+
+function getNotation(report){
+    var result = 3;
+    for (x in report) {
+        if (x == "title" || x == "description" || x == "headings") {
+            if (report[x].result == false) return 0;
+        }
+        if (x == "link-img-alt" || x == "img-alt" || x == "microformats") {
+            if (report[x].result == false) return 1;
+        }
+    }
+    return result;
+}
+
+    
+function main(){
+    var deferred = Q.defer();
+    // Prod status
+    timeCounter.register('page crawler');
+    request.get({ url: conf.dashku.url }, function (err, response) {
+        var data = {};
+        if (err) deferred.reject(err);
+        crawlability(confJSON).then(
+            function (report) {
+                for (x in report) {
+                    data.url = x;
+                    data.rules = report[x];
+                }
+                
+                data.note = getNotation(report[x]);
+                //request.post({ url: conf.dashku.url, body: data, json: true });
+                deferred.resolve({
+                    "url": data.url,
+                    "rules": data.rules,
+                    "note": data.note,
+                    "value": response.statusCode,
+                    "delay": timeCounter.getFormatedDelay('page crawler')
+                });
+                console.log(getNotation(report[x]));
+            },
+            function (err) {
+                console.log('ERROR');
+            }
+        );
+    });
+        
+    return deferred.promise;
+         
+}
+    
+if(!module.parent) { main(); } else { module.exports = main; }
