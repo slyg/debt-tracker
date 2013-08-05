0be54cc817c8c9bc969305b868e5b53c3fb10ca1
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-08-05 17:01:02 +0200

    updating ui scripts and style

--- viadeo-trunk-less-parsing/client/widget.js
+++ viadeo-trunk-less-parsing/client/widget.js
@@ -1,35 +1,39 @@
 var widget = this.widget;
 
 this.on('load', function(){
-  
-  widget.on("click", ".dot", function(){
-    var ref = this.id;
-    widget.find('#csspath').val(ref);
-  });
-
+	widget.parent().removeClass('red-pulse');
 });
 
 this.on('transmission', function(res){
   
-  var 
+ 	var 
       data = res.data,
       len = data.length,
-      dom = widget.find('.container'),
+      dom = widget.find('.errors'),
       html = "",
       validClass = "",
       statusClass = ""
-  ;
+ 	;
   
-  while(len--){
+  widget.parent().removeClass('red-pulse');
+  widget.find(".message").hide().fadeIn('slow').text((new Date()).toLocaleTimeString());
+  widget.find(".status").text("OK").removeClass("ko");
+  dom.empty().hide();
+  
+  while(len--){ console.log(data[len]['reference']);
     
-    validClass = (data[len]["valid"] != true) ? "invalid red-pulse" : "";
+    validClass = (data[len]["valid"] != true) ? "invalid" : "";
     statusClass = (data[len]["found"] != true) ? "unreached" : "";
     
-    html += "<span class=\"dot " + validClass + " " + statusClass + " \" id=\"" + data[len]["reference"] + "\"></span>";
+    if(data[len]["valid"] === false || data[len]["found"] === false) { console.log('coucou')
+    	widget.parent().addClass('red-pulse');
+      widget.find(".status").text("KO").addClass("ko");
+      html += "<li class=\"" + validClass + " " + statusClass + " \">" + data[len]["reference"] + "</li>";
+    }
     
   }
-  
-  dom.empty().html(html);
+ 
+  dom.html(html).fadeIn('slow');
   
   
 });
