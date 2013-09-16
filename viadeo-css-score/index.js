82dacd0b51f4eee1f501d25ed8453b6ab3a2a89d
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-09-16 18:02:11 +0200

    first commit of css-specificity widget

---
+++ viadeo-css-score/index.js
@@ -0,0 +1,35 @@
+
+    var 
+        path        = require('path'),
+        Q           = require('q'),
+        util        = require('util')
+    ;
+    
+    function main(){
+    
+        var 
+            deferred    = Q.defer()
+        ;
+        
+        Q   
+            .fcall(function(){ return true; })
+            .then(require('./../viadeo-trunk-less-parsing/assets/parseWro'))          // returns wro.xml as js object -> wro
+            .then(require('./../viadeo-trunk-less-parsing/assets/getLessPackages'))   // returns raw less packages & classpath & group-ref dependencies
+            .then(require('./../viadeo-trunk-less-parsing/assets/putAbsolutePath'))   // returns wro w/classpath replaced by rel to process root path, ie for common resources
+            .then(require('./../viadeo-trunk-less-parsing/assets/evaluateGroupRef'))  // returns wro with group-ref replaced by original group
+            .then(require('./assets/lessCompilation'))   // compiles groups and returns report
+            .then(function(report){
+                console.log( util.inspect(report, false, null) );
+                deferred.resolve({"data": report});
+            }, function(err){
+                deferred.reject(err);
+            })
+        ;
+        
+        return deferred.promise;
+        
+    }
+    
+    if(!module.parent) { main(); } else { module.exports = main; }
+    
+    
