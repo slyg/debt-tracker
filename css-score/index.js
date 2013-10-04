24d8d9502cc680ebcf7f5b08df84e35658f391b5
Sylvain Faucherand <syl.faucherand@gmail.com>
2013-10-04 17:45:03 +0200

    reorganize and rename the whole mess

--- css-score/index.js
+++ css-score/index.js
@@ -14,11 +14,11 @@
         
         Q   
             .fcall(function(){ return true; })
-            .then(require('./../viadeo-trunk-less-parsing/assets/parseWro'))          // returns wro.xml as js object -> wro
-            .then(require('./../viadeo-trunk-less-parsing/assets/getLessPackages'))   // returns raw less packages & classpath & group-ref dependencies
-            .then(require('./../viadeo-trunk-less-parsing/assets/putAbsolutePath'))   // returns wro w/classpath replaced by rel to process root path, ie for common resources
-            .then(require('./../viadeo-trunk-less-parsing/assets/evaluateGroupRef'))  // returns wro with group-ref replaced by original group
-            .then(require('./../viadeo-trunk-less-parsing/assets/lessCompilation'))   // compiles groups and returns report
+            .then(require('./../less-parsing/assets/parseWro'))          // returns wro.xml as js object -> wro
+            .then(require('./../less-parsing/assets/getLessPackages'))   // returns raw less packages & classpath & group-ref dependencies
+            .then(require('./../less-parsing/assets/putAbsolutePath'))   // returns wro w/classpath replaced by rel to process root path, ie for common resources
+            .then(require('./../less-parsing/assets/evaluateGroupRef'))  // returns wro with group-ref replaced by original group
+            .then(require('./../less-parsing/assets/lessCompilation'))   // compiles groups and returns report
             .then(require('./assets/computeStatistics'))
             .then(function(report){
 
--- viadeo-prod-status/acqui/index.js
+++
@@ -1,1 +0,0 @@
-module.exports = require('./../common')('http://join.viadeo.com');
